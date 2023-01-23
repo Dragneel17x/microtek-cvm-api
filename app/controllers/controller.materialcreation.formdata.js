1b208d8e1f62206de581dea015db972a854115c8
Dragneel17x <59982369+Dragneel17x@users.noreply.github.com>
2023-01-23 17:49:58 +0530

    Update controller.materialcreation.formdata.js

--- app/controllers/controller.materialcreation.formdata.js
+++ app/controllers/controller.materialcreation.formdata.js
@@ -17,3 +17,43 @@ exports.getPlantData = async (req,res)=>{
 	res.status(200).send(send_data);
     console.log(send_data);
 }
+/* exports.getStorageLocation = async (req,res)=>{
+
+	const getPlantData = await sequelize.query(`select * from plant_master`, { type: QueryTypes.SELECT });
+	const send_data = {
+		status: 200,
+		data: getPlantData,
+		message: "data fetched successfully",
+	};
+	res.status(200).send(send_data);
+    console.log(send_data);
+} */
+exports.getStorageLocation= async (req, res) => {
+	const { plant_name } = req.body;
+	if (!plant_name) {
+		const send_data = {
+			status: 400,
+			message: "Plant Name Required",
+		};
+		res.status(400).send(send_data);
+		return;
+	}
+
+    const getPlantCode = await sequelize.query(`select plant_code from plant_master where plant_name = '${plant_name}'`, {type:QueryTypes.SELECT});
+	const getStorageLocationMapping = await sequelize.query(`select * from storage_location_master where plant_code = '${getPlantCode}'`, { type: QueryTypes.SELECT });
+	console.log(getStorageLocationMapping);
+	if (!getStorageLocationMapping?.length) {
+		const send_data = {
+			status: 200,
+			message: "NO DATA FOUND",
+		};
+		res.status(200).send(send_data);
+		return;
+	}
+	const send_data = {
+		status: 200,
+		data: getStorageLocationMapping[0],
+		message: "data fetched successfully",
+	};
+	res.status(200).send(send_data);
+};
