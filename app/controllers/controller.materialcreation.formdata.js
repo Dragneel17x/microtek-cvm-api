cef34b407cb29c447a3f7b1a31f88c305a19a038
Dragneel17x <59982369+Dragneel17x@users.noreply.github.com>
2023-01-24 12:50:22 +0530

    .

--- app/controllers/controller.materialcreation.formdata.js
+++ app/controllers/controller.materialcreation.formdata.js
@@ -58,13 +58,12 @@ exports.getStorageLocation= async (req, res) => {
 };
 
 exports.getMatSalesOrg = async(req,res)=>{
-     const getMatSalesOrg = await sequelize.query(`select * from mat_sales_org`, {type: QueryTypes.SELECT})
+     const getMatSalesOrg = await sequelize.query(`select * from mat_sales_org`, {type: QueryTypes.SELECT});
     
      const send_data = {
         status: 200,
         data: getMatSalesOrg,
         message: "Data Fetched Succesfully"
      }
-     console.log(res);
-     res.status(200).data(send_data);
+     res.status(200).send(send_data);
 }
