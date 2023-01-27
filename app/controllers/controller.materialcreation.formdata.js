0bd608cb905abbec7b8b85a353036b9f05c505b3
Dragneel17x <59982369+Dragneel17x@users.noreply.github.com>
2023-01-27 15:23:05 +0530

    ..

--- app/controllers/controller.materialcreation.formdata.js
+++ app/controllers/controller.materialcreation.formdata.js
@@ -9,6 +9,7 @@ const flag = require("../Flag");
 exports.getPlantData = async (req,res)=>{
 
 	const getPlantData = await sequelize.query(`select * from plant_master`, { type: QueryTypes.SELECT });
+    console.log(getPlantData);
 	const send_data = {
 		status: 200,
 		data: getPlantData,
@@ -209,4 +210,72 @@ exports.materialFormApplrovals = async (req, res) => {
 		message: "Data Fetched Successfully",
 	};
 	res.status(200).send(send_data);
+};
+
+exports.postFormData = async (req, res) => {
+	const formData = req.body;
+	console.log(req.body);
+/* 	console.log(req.files);
+	const reqFile = req.files;
+	console.log(reqFile);
+	if (!reqFile.DAPF) {
+		reqFile.DAPF = [];
+	}
+	if (!reqFile.PAN_Image) {
+		reqFile.PAN_Image = [];
+	}
+	if (!reqFile.GST_Image) {
+		reqFile.GST_Image = [];
+	}
+	if (!reqFile.declaration) {
+		reqFile.declaration = [];
+	}
+	if (!reqFile.blank_cheque) {
+		reqFile.blank_cheque = [];
+	}
+ */
+	if (!formData) {
+		console.log("data not recieved");
+		return;
+	}
+
+	/* if (!(formData.vendor_group && formData.vendor_name && formData.vendor_address && formData.vendor_address_op1 && formData.co_person && formData.city && formData.postal_code && formData.country && formData.company_code && formData.bank_acc_no && formData.pay_term && formData.name_on_acc && formData.ifsc_code && formData.employee_id)) {
+		const send_data = {
+			status: "401",
+			message: "all parameters required",
+		};
+
+		res.status(401).send(send_data);
+	} */
+	if(false){}
+	else {
+		/* console.log(Object.keys(formdata)); */
+
+		try {
+			const approval_data = await sequelize.query(`select * from approval_matrix where approval_type = 'vendor_form' order by approval_level;`, { type: QueryTypes.SELECT });
+			console.log(approval_data);
+
+			const data = await sequelize.query(`insert into material_creation_form_data (mat_type,mat_logic_no, plant_name, plant_code, storage_location, storage_location_code,sales_organization,sales_organization_code,mat_short_desc,dist_channel,base_unit,base_unit_code,mat_long_desc,dist_channel,dist_channel_code,mat_group, mat_grp_code,division,division_code,mat_price_grp,mat_price_grp_code,purchasing_grp,purchasing_grp_code,gr_proc_time,hsn_code,serial_no_profile,quality_inspection,quality_inspection_code,price_control,price_control_desc, valutaion_type) values ('${formData.vendor_group}','${formData.vendor_name}','${formData.vendor_name_op1}','${formData.vendor_address}','${formData.vendor_address_op1}','${formData.vendor_address_op2}','${formData.vendor_address_op3}','${formData.district}','${formData.state_code}','${formData.city}','${formData.postal_code}','${formData.country}','${formData.co_person}','${formData.mobile_no}','${formData.email_id}','${formData.company_code}','${formData.pay_term}','${formData.gstin}','${formData.pan}','${formData.employee_id}', '${formData.employee_id}','${formData.order_currency}','${formData.name_on_acc}','${formData.bank_acc_no}','${formData.ifsc_code}','${formData.purchasing_org}','${formData.witholding_tax}')`, { type: QueryTypes.INSERT });
+			console.log(data);
+
+			const postData = await sequelize.query(`INSERT INTO file_path_mapping (form_data_id , form_type, blank_cheque, GST_Image, PAN_Image, declaration, DAPF) VALUES (${data[0]} , 'vendor_form','${reqFile?.blank_cheque[0]?.path.replace(/\\/g, "/").replace(/^dist\//, "")}', '${reqFile?.GST_Image[0]?.path.replace(/\\/g, "/").replace(/^dist\//, "")}', '${reqFile?.PAN_Image[0]?.path.replace(/\\/g, "/").replace(/^dist\//, "")}','${reqFile?.declaration[0]?.path.replace(/\\/g, "/").replace(/^dist\//, "")}', '${reqFile?.DAPF[0]?.path.replace(/\\/g, "/").replace(/^dist\//, "")}')`, { type: QueryTypes.INSERT });
+			console.log("postdata" + postData);
+			approval_data.map(async (item,i) => {
+				const insert_approval = await sequelize.query(`INSERT INTO approval_inbox ( request_type, request_id, approval_level, applied_by, approver_employee_id, status, created_by, updated_by) VALUES ('vendor_form',${data[0]},'${item.approval_level}','${formData.employee_id}','${item.approver_employee_id}','${i==0? "pending":"future_approval"}','${formData.employee_id}','${formData.employee_id}');`, { type: QueryTypes.INSERT });
+				console.log(insert_approval);
+			});
+			const send_data = {
+				status: 200,
+				message: "data Insereted successfully",
+			};
+			res.status(200).send(send_data);
+		} catch (error) {
+			console.log(error);
+			const send_data = {
+				status: 500,
+				message: "Unexcexpted Error Occured",
+			};
+			res.status(500).send(send_data);
+		}
+	}
 };
