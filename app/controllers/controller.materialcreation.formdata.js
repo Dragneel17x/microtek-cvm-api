4e923bd72dfe1af18127fbae683235de249bbffa
Dragneel17x <59982369+Dragneel17x@users.noreply.github.com>
2023-01-27 12:08:09 +0530

    ..

--- app/controllers/controller.materialcreation.formdata.js
+++ app/controllers/controller.materialcreation.formdata.js
@@ -178,7 +178,8 @@ exports.getValuationType = async (req,res)=>{
 		return;
 	}
 
-	const getValuationType = await sequelize.query(`select * from valuation_type_master where material_type = '${mat_type}'`, { type: QueryTypes.SELECT });
+    const getMaterialType = await sequelize.query(`select material_type from material_type_master where mat_desc = '${mat_type}'`, {type:QueryTypes.SELECT});
+	const getValuationType = await sequelize.query(`select * from valuation_type_master where material_type = '${getMaterialType}'`, { type: QueryTypes.SELECT });
 	const send_data = {
 		status: 200,
 		data: getValuationType,
